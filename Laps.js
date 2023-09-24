ea005e95f33dde2f74248438bdcaac7966d512cb
Ronald <46291029+GonzalezRon@users.noreply.github.com>
2023-09-23 23:14:29 -0700

    Update Laps.js

--- Laps.js
+++ Laps.js
@@ -1,72 +1,57 @@
-import {React, useCallback, useMemo, useRef} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native'; 
+import {React, useRef} from 'react';
+import {View, StyleSheet} from 'react-native'; 
+import LapsRender from './LapsRender';
+import {FlashList} from '@shopify/flash-list'
 
 
 
-function laps ({ width, height, lapsList}){ 
 
 
+function laps ({ width, height, lapsList, size}){ 
+
+    const screenHeight = height
     const screenWidth = width
-    
-    const renderItem = useCallback(({item, index})=>{
-        console.log("this statement shouldnt show too often")
-        return (
-            <View style = {styles.LapFrame}>
-                <Text style={styles.lapText}>Lap{index + 1}</Text>
-                <Text style={styles.lapText}>{item}</Text>
-            </View> 
-        )
-    },[])
 
-    const keyExtractor = useCallback((index)=>{
-        return()=> index
-   },[])
+    const flashRef = useRef(null)
+
+
+    const renderItem = ({item, index})=>{
+        return (
+            <LapsRender 
+            item = {item} 
+            size = {size}
+            index ={index + 1}/> // add one to remove index 
+        )
+    }
 
     return (
         <View style={styles.mainView}>
-            <View style={{width: screenWidth * .9}}>
-                <FlatList
+            <View style={{width: screenWidth * .9, minHeight: screenHeight/3.99}}>
+               
+                <FlashList
+                ref={flashRef}
+                extraData={size}
                 data={lapsList}
                 renderItem={renderItem}
-                key={keyExtractor} 
+                estimatedItemSize={10}
+                inverted={false}
+                onContentSizeChange={()=>{
+                    flashRef.current.scrollToIndex({index: size-1})
+                }}
                 /> 
+
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    LapFrame : {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        borderWidth:16,
-        borderBottomWidth: .17,
-        borderBottomColor: '#F9F6EE'
-    }, 
-    lapText: {
-        color:'#F9F6EE',
-        fontSize: 18, 
-        borderBottomWidth: 8,
-    },
     
     mainView: {
         backgroundColor: 'black',
-         flex:.95,
     }, 
-
 })
 
 export default laps;
 
 
-
-   {/* {laps.map(
-                        (lap, index)=> {
-                            
-                            return(
-                            <View style = {styles.LapFrame} key={index}>
-                                <Text style={styles.lapText}>Lap{index + 1}</Text>
-                                <Text style={styles.lapText}>{lap}</Text>
-                            </View> 
-                        )
-                    })} */}
